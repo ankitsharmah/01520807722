2751bcba44c2d4216990e25c03eaf0c13e3fca3c
ankitsharma06 <asrover9810@gmail.com>
2024-07-05 12:19:52 +0530

    updated config

--- src/main/java/com/example/Avergaecalcluator/config/RestTemplateConfig.java
+++ src/main/java/com/example/Avergaecalcluator/config/RestTemplateConfig.java
@@ -24,7 +24,7 @@ public class RestTemplateConfig {
     private ClientHttpRequestInterceptor authInterceptor() {
         return (request, body, execution) -> {
             // Add the authorization header (e.g., Bearer token)
-            request.getHeaders().set("Authorization", "Bearer your-static-token");
+            request.getHeaders().set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIwMTYyMzI4LCJpYXQiOjE3MjAxNjIwMjgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjQ5OWI2ZTUyLWU0ZTktNDBhMC05OGE2LTJjNGU3ZDBmZGI0MiIsInN1YiI6ImFzcm92ZXIyMDI5QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6IlRlc3RpbmciLCJjbGllbnRJRCI6IjQ5OWI2ZTUyLWU0ZTktNDBhMC05OGE2LTJjNGU3ZDBmZGI0MiIsImNsaWVudFNlY3JldCI6InprTkdQV0tHVkJzZlVod1YiLCJvd25lck5hbWUiOiJhbmtpdCBrdW1hciIsIm93bmVyRW1haWwiOiJhc3JvdmVyMjAyOUBnbWFpbC5jb20iLCJyb2xsTm8iOiIwMTUyMDgwNzcyMiJ9.39LLyjDTK6Xa8Oj_AleXq-FCVuCcbj__7-yxC3HvJKc");
             return execution.execute(request, body);
         };
     }
